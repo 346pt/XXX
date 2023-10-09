1ecdf05d7d698d5b2e341bbf55a20f5fa218e8ff
The4yo <eugshpak@yandex.ru>
2023-10-09 19:00:28 +0300

    Добавьте файлы проекта.

---
+++ X/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        int max = 0;
+        int default1 = 0;
+
+        using (StreamReader a = new StreamReader("C:\\Users\\User\\Desktop\\no.txt"))
+        {
+            string b = a.ReadToEnd();
+
+            for (int i = 0; i < b.Length; i++)
+            {
+                if (b[i] == 'X')
+                {
+                    default1++;
+                    if (default1 > max)
+                        max = default1;
+                }
+                else
+                {
+                    default1 = 0;
+                }
+            }
+        }
+
+        Console.WriteLine(max);
+    }
+}
